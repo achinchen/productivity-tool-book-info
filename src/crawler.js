b22827116a4ca348b88aa547fa6eda142d326883
Chin <greeting.to.chin@gmail.com>
2022-01-31 08:25:37 +0800

    feature(processor): tidy up the process of creating read note

--- src/crawler.js
+++ src/crawler.js
@@ -1,17 +1,16 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const { argv } = require('process');
-const getBookInfo = require('./utils')
 const url = argv[2]
 
 async function getHTML() {
   try {
     const { stdout } = await exec(`curl ${url}`);
-    const result = await getBookInfo(stdout)
-    console.log(result)
+    return stdout
   } catch (err) {
     console.error(err);
   };
 };
 
-getHTML();
+
+module.exports = getHTML
